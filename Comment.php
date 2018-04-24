ca6f5d58ba0333214d1127506a1de63ff347a272
Michael Figueroa <mfigueroa14@cnm.edu>
2018-04-24 08:53:21 -0600

    Snap Comment

---
+++ Comment.php
@@ -0,0 +1,52 @@
+<?php
+class comment {
+	public $commentSaltiness;
+	public $commentText;
+	public $commentUsername;
+
+	/**
+	 * @return mixed
+	 */
+	public function getCommentSaltiness() {
+		return $this->commentSaltiness;
+}
+
+	/**
+	 * @param $commentSaltiness
+	 * @return string
+	 */
+	public function setCommentSaltiness($commentSaltiness): float {
+		$this->commentSaltiness = $commentSaltiness;
+}
+
+	/**
+	 * @return mixed
+	 */
+	public function getCommentText(){
+		return $this->commentText;
+}
+
+	/**
+	 * @param $setCommentText
+	 * @return float
+	 */
+	public function setCommentText($setCommentText): float {
+		$this->commentText = $commentText;
+}
+
+	/**
+	 * @return mixed
+	 */
+	public function getCommentUsername() {
+		return $this->commentUsername;
+}
+
+	/**
+	 * @param $setCommentUsername
+	 * @return float
+	 */
+	public function setCommentUsername($setCommentUsername): float {
+		$this->commentUsername;
+}
+
+}
